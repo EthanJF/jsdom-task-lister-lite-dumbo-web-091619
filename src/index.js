21c0f28d292153ba570a461176ca7daa4b2461a6
EthanJF <ethanjosephfournier@gmail.com>
2019-10-29 16:39:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let submit = document.querySelectorAll("input")[1]
+
+  submit.addEventListener("click", function(event){
+    event.preventDefault();
+    let taskText = document.querySelector("#new-task-description").value
+    let newToDo = document.createElement("li")
+    newToDo.innerHTML = taskText
+    let toDoList = document.querySelector("#tasks")
+    toDoList.append(newToDo)
+    let deleteButton = document.createElement("button")
+    deleteButton.innerText = "Delete"
+    newToDo.append(deleteButton)
+    
+    deleteButton.addEventListener("click", function(event){
+      newToDo.remove()
+    })
+  })
 });
