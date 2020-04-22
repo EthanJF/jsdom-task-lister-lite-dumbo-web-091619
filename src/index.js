c8fe5d07c5a8e57baf7f69a4ea5a3f406b50c6a8
EthanJF <ethanjosephfournier@gmail.com>
2020-04-22 16:43:36 -0400

    updated code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let submit = document.querySelectorAll("input")[1]
+  const submit = document.querySelectorAll("input")[1]
 
   submit.addEventListener("click", function(event){
     event.preventDefault();
-    let taskText = document.querySelector("#new-task-description").value
-    let newToDo = document.createElement("li")
-    newToDo.innerHTML = taskText
-    let toDoList = document.querySelector("#tasks")
+    const taskText = document.querySelector("#new-task-description")
+    const newToDo = document.createElement("li")
+    newToDo.textContent = taskText.value
+    const toDoList = document.querySelector("#tasks")
     toDoList.append(newToDo)
-    let deleteButton = document.createElement("button")
+
+    taskText.value = ""
+
+    const deleteButton = document.createElement("button")
     deleteButton.innerText = "Delete"
     newToDo.append(deleteButton)
     
-    deleteButton.addEventListener("click", function(event){
+    deleteButton.addEventListener("click", function(){
       newToDo.remove()
     })
   })
